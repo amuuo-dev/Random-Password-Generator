ee74b1389f6fd381f5032c5197256d1e79405725
Anthony Muuo <132995647+amuuo-dev@users.noreply.github.com>
2023-09-04 07:34:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d",
+"e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",
+"w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",
+":",";","<",">",".","?",
+"/"];
+let stageEl=document.getElementById("passOne")
+let copyButtonEl=document.getElementById("copyButton")
+function getRandomPassword(){
+    let Password='';
+    for(let i=0; i<15; i++){
+    let randomPasswordA=Math.floor(Math.random()*characters.length)
+    stageEl.textContent=Password+=characters[randomPasswordA]
+}
+}
+
