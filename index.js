a94a80d332e44c0ea15221033b78d14db754e682
amuuo-dev <anthonmuuo92@gmail.com>
2024-01-23 09:04:56 +0300

    added copy function on copy icon in the index.js

--- index.js
+++ index.js
@@ -6,11 +6,40 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
 ":",";","<",">",".","?",
 "/"];
 let stageEl=document.getElementById("passOne")
-function getRandomPassword(){
-    let Password=''
+ let yourInputEl=document.getElementById("yourInputId")
+ function getRandomPassword(){
+    let password=""
     for(let i=0; i<15; i++){
-    let randomPasswordA=Math.floor(Math.random()*characters.length)
-    stageEl.textContent=Password+=characters[randomPasswordA]
-}
-}
+        let randomIndex=Math.floor(Math.random()*characters.length)
+        password+=characters[randomIndex]
+    }
+    
+  // Set the generated password in the input field and display it
+    yourInputEl.value = password
+ }
+ function copyPassword(){
+    // Get the input element by its ID
+    let inputEL=document.getElementById("yourInputId")
+    //select text inside the input element
+    inputEL.select()
+    try{
+        //execute copy command
+        document.execCommand("copy")
+           
+        // Update the content and display of the copied message
+        let copiedMessage = document.querySelector('.copy-password button');
+        copiedMessage.classList.add("active");
 
+        // You can add a delay to hide the message after a certain time (e.g., 2 seconds)
+        setTimeout(function() {
+            copiedMessage.classList.remove("active");
+        }, 2000);
+
+        window.getSelection().removeAllRanges();
+        
+    }
+    catch(err){
+        // Handle any errors that may occur during copying
+        console.error('Unable to copy text:', err);
+    }
+ }
